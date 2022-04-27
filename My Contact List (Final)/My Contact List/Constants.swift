8b966f619c258fafaba258cac78f9c4255cd12ee
LucasCSom <97651621+LucasCSom@users.noreply.github.com>
2022-04-27 18:32:36 -0400

    Add files via upload

---
+++ My Contact List (Final)/My Contact List/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  My Contact List
+//
+//  Created by user218545 on 4/23/22.
+//  Copyright © 2019 Learning Mobile Apps. All rights reserved.
+//
+
+import Foundation
+struct Constants  {
+    static let kSortField = "sortField"
+    static let kSortDirectionAscending = "sortDirectionAscending"
+}
---
+++ My Contact List (Final)/My Contact List/MapPoint.swift
@@ -0,0 +1,26 @@
+//
+//  MapPoint.swift
+//  My Contact List
+//
+//  Created by user218545 on 4/23/22.
+//  Copyright © 2019 Learning Mobile Apps. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class MapPoint: NSObject, MKAnnotation{
+    var title: String?
+    var subtitle: String?
+    var latitude: Double
+    var longitude: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
